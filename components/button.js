5d29d2762b597d3a9cdac29dcf0854659a392962
Dan <dan@ariesliving.com>
2020-08-11 13:47:30 -0400

    add reztrip url

--- components/button.js
+++ components/button.js
@@ -32,16 +32,16 @@ const cachedStyles = css`
     height: 2.81rem;
     line-height: 2.8rem;
     border-radius: var(--radius);
-    background-color: var(--accents-1);
-    box-shadow: 0 4px 14px 0 rgba(0, 154, 148, 0.39);
+    background-color: var(--accents-3);
+    box-shadow: 0 4px 14px 0 rgba(239, 170, 156, 0.39);
     color: white;
   }
   .btn.invert:hover {
     background: var(--accents-1);
-    box-shadow: 0 6px 20px rgba(0, 154, 148, 0.23);
+    box-shadow: 0 6px 20px rgba(239, 170, 156, 0.23);
   }
   .btn.invert:active {
-    background: var(--accents-2);
+    background: var(--accents-1);
   }
   .btn.invert.outline {
     background: var(--white);
